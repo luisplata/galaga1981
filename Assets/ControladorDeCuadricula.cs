863994317b2c16723b4289466cdff9a28a8b199b
Luis Plata <www.luisplata@gmail.com>
2020-06-11 23:13:00 -0500

    Primera prueba de ordenamiento dinamico de enemigos

---
+++ Assets/ControladorDeCuadricula.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorDeCuadricula : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public bool estaDisponible = true;
+
+}
