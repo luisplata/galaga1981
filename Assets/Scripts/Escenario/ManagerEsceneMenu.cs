7783a16d8ca9b35af654a5539bdfc5bb5bf89f21
PeryLoth <www.luisplata@gmail.com>
2023-07-02 00:20:03 -0500

    Load new sprites for new game, new feature in main menu: Message from creator

--- Assets/Scripts/Escenario/ManagerEsceneMenu.cs
+++ Assets/Scripts/Escenario/ManagerEsceneMenu.cs
@@ -7,7 +7,8 @@ using UnityEngine.SceneManagement;
 
 public class ManagerEsceneMenu : MonoBehaviour
 {
-    public TextMeshProUGUI clickToContinue, ranking;
+    [SerializeField] private string urlToMessage;
+    public TextMeshProUGUI ranking, message;
     private float parpadeo = 1;
     private float detalTime = 0;
     private bool parpadeobol = false;
@@ -15,27 +16,18 @@ public class ManagerEsceneMenu : MonoBehaviour
     {
         ranking.text = "";
         StartCoroutine(GetRequest("https://juegos.peryloth.com/api/" + "score/best/galaga"));
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        detalTime += Time.deltaTime;
-        if(detalTime >= parpadeo || (detalTime >= parpadeo/2 && parpadeobol))
-        {
-            detalTime = 0;
-            clickToContinue.gameObject.SetActive(parpadeobol);
-            parpadeobol = !parpadeobol;
-        }
-        if (Input.GetKey(KeyCode.Joystick1Button1))
-        {
-            InicioGame();
-        }
+        StartCoroutine(GetRequestMessage(urlToMessage));
     }
 
     public void InicioGame()
     {
         PlayerPrefs.SetInt("score", 0);
         SceneManager.LoadScene("02_Game");
+    }
+    public void InicioNewGame()
+    {
+        PlayerPrefs.SetInt("newScore", 0);
+        SceneManager.LoadScene("022_Game");
     }
 
     IEnumerator GetRequest(string uri)
@@ -60,6 +52,26 @@ public class ManagerEsceneMenu : MonoBehaviour
                 Score[] s = JsonHelper.FromJson<Score>(webRequest.downloadHandler.text);
                 ranking.text += "1 - "+ s[0].score+ " - "+ s[0].nombre;
                 Debug.Log("1 - " + s[0].score + " - " + s[0].nombre);
+            }
+        }
+    }
+    IEnumerator GetRequestMessage(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+
+                message.text = "Error en el servidor";
+            }
+            else
+            {
+                message.text = "";
+                Debug.Log(")))))" + webRequest.downloadHandler.text);
+                message.text = webRequest.downloadHandler.text;
             }
         }
     }
---
+++ Assets/Scripts/Escenario/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private Button buttonConfiguration, buttonBackToMainMenu;
+    [SerializeField] private GameObject panelConfiguration;
+
+    private void Start()
+    {
+        buttonConfiguration.onClick.AddListener(OpenConfiguration);
+        buttonBackToMainMenu.onClick.AddListener(BackToMainMenu);
+        panelConfiguration.SetActive(false);
+    }
+
+    private void BackToMainMenu()
+    {
+        panelConfiguration.SetActive(false);
+    }
+
+    private void OpenConfiguration()
+    {
+        panelConfiguration.SetActive(true);
+    }
+}
